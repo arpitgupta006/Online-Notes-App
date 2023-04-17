4ebd88c8bab05d74a02690015ad4c09dc06b7371
ARPIT <arpitdude006@gmail.com>
2023-04-17 21:05:04 +0530

    Initial commit

---
+++ app/src/main/java/com/example/onlinenotesapp/models/NoteRequest.kt
@@ -0,0 +1,6 @@
+package com.example.onlinenotesapp.models
+
+data class NoteRequest(
+    val title: String,
+    val description: String
+)
---
+++ app/src/main/java/com/example/onlinenotesapp/ui/login/RegisterFragment.kt
@@ -0,0 +1,30 @@
+package com.example.onlinenotesapp.ui.login
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.onlinenotesapp.R
+import com.example.onlinenotesapp.databinding.FragmentRegisterBinding
+
+class RegisterFragment : Fragment() {
+
+    private var _binding : FragmentRegisterBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentRegisterBinding.inflate(inflater , container , false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+}
